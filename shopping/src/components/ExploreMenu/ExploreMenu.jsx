742134a6d6884bb3fbf34e3b0992fea0c28cb23b
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-03-03 17:36:27 +0530

    new changes

--- shopping/src/components/ExploreMenu/ExploreMenu.jsx
+++ shopping/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,23 +2,38 @@ import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/frontend_assets/assets'
 
-const ExploreMenu = ({category , setCategory}) => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
-      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes . Our mission is to satisfy your cravings and elevate your during experience , one delicious meal at a time </p>
+
+      <p className='explore-menu-text'>
+        Choose from a diverse menu featuring a delectable array of dishes.
+        Our mission is to satisfy your cravings and elevate your dining experience,
+        one delicious meal at a time.
+      </p>
+
       <div className='explore-menu-list'>
-       {
-         menu_list.map((items , index)=>{
-            return (
-                <div  onClick={()=>setCategory(prev=>prev===items.menu_image?"All":items.menu_name)} key={index} className='explore-menu-list-item'>
-                    <img className={category===items.menu_name?"active":""} src={items.menu_image} alt="" />
-                    <p>{items.menu_name}</p>
-                </div>
-            )
-         })
-       }
+        {menu_list.map((items, index) => (
+          <div
+            key={index}
+            className='explore-menu-list-item'
+            onClick={() =>
+              setCategory(prev =>
+                prev === items.menu_name ? "All" : items.menu_name
+              )
+            }
+          >
+            <img
+              className={category === items.menu_name ? "active" : ""}
+              src={items.menu_image}
+              alt={items.menu_name}
+            />
+            <p>{items.menu_name}</p>
+          </div>
+        ))}
       </div>
+
       <hr />
     </div>
   )
